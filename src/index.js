7d6c9789c6fb1de32bdfea5f04e817543b02395d
christina172 <117263200+christina172@users.noreply.github.com>
2023-02-28 13:29:40 +0300

    Add function to display weather on page and functions to convert time

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import './styles.css';
+import fromUnixTime from 'date-fns/fromUnixTime';
 
 console.log("Hello!");
+
+//https://date-fns.org/v2.29.3/docs/fromUnixTime
+
+//https://openweathermap.org/current
+//https://openweathermap.org/weather-conditions
 
 async function getWeather(city) {
     try {
@@ -19,10 +25,11 @@ const getObject = function (weather) {
         const obj = {
             name: weather.name,
             country: weather.sys.country,
-            dt: weather.dt,
+            dt: convertToLocalDate(weather.dt),
             timezone: weather.timezone,
-            sunrise: weather.sys.sunrise,
-            sunset: weather.sys.sunset,
+            time: convertDate(weather.dt + weather.timezone),
+            sunrise: convertTime(weather.sys.sunrise + weather.timezone),
+            sunset: convertTime(weather.sys.sunset + weather.timezone),
             main: weather.weather[0].main,
             description: weather.weather[0].description,
             temp: weather.main.temp,
@@ -33,12 +40,14 @@ const getObject = function (weather) {
             clouds: weather.clouds.all,
         }
         console.log(obj);
+        return obj;
     } else {
         const obj = {
             cod: weather.cod,
             message: weather.message,
         }
         console.log(obj.message);
+        return obj;
     }
 };
 
@@ -50,10 +59,74 @@ const input = document.querySelector("input");
 const find = document.querySelector(".find");
 find.addEventListener("click", () => {
     console.log(input.value);
-    getWeather(input.value).then(getObject).catch(getError);
+    getWeather(input.value).then(getObject).then(displayWeather).catch(getError);
 });
 
 // getWeather("Grodno").then(getObject).catch(getError);
 // getWeather("sljgbdjf").then(getObject).catch(getError);
 // getWeather("").then(getObject).catch(getError);
+
+const card = document.querySelector(".card");
+const nameAndTemp = document.querySelector(".name-and-temp");
+const name = document.querySelector(".name");
+const temp = document.querySelector(".temp");
+const weatherAndImage = document.querySelector(".weather-and-image");
+const weather = document.querySelector(".weather");
+const main = document.querySelector(".main");
+const description = document.querySelector(".description");
+const image = document.querySelector(".image");
+const feelsLike = document.querySelector(".feels-like");
+const cloudiness = document.querySelector(".cloudiness");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const visibility = document.querySelector(".visibility");
+const sun = document.querySelector(".sun");
+const sunrise = document.querySelector(".sunrise");
+const sunset = document.querySelector(".sunset");
+const cityTime = document.querySelector(".city-time");
+const time = document.querySelector(".time");
+const button = document.querySelector(".button");
+const toggle = document.querySelector(".toggle");
+
+function displayWeather(obj) {
+    name.textContent = `${obj.name}, ${obj.country}`;
+    temp.textContent = `${obj.temp} °C`;
+    main.textContent = obj.main;
+    description.textContent = obj.description.charAt(0).toUpperCase() + obj.description.slice(1);
+    feelsLike.textContent = `Feels like: ${obj.feels_like} °C`;
+    cloudiness.textContent = `Cloudiness: ${obj.clouds}%`;
+    humidity.textContent = `Humidity: ${obj.humidity}%`;
+    wind.textContent = `Wind speed: ${obj.speed} m/s`;
+    visibility.textContent = `Visibility: ${obj.visibility} m`;
+    sunrise.textContent = `Sunrise: ${obj.sunrise}`;
+    sunset.textContent = `Sunset: ${obj.sunset}`;
+    cityTime.textContent = `Time in ${obj.name}: ${obj.time}`;
+    time.textContent = `Time of request: ${obj.dt}`;
+};
+
+function chooseImage() {
+
+};
+
+function convertTime(unix_timestamp) {
+    let date = new Date(unix_timestamp * 1000);
+    let timeString = date.toUTCString();
+    let formattedTime = timeString.slice(-12, -7);
+    return formattedTime;
+};
+
+function convertDate(unix_timestamp) {
+    let date = new Date(unix_timestamp * 1000);
+    let timeString = date.toUTCString();
+    let formattedTime = timeString.slice(0, -3);
+    return formattedTime;
+};
+
+function convertToLocalDate(unix_timestamp) {
+    let date = new Date(unix_timestamp * 1000);
+    return date;
+};
+
+
+
 
